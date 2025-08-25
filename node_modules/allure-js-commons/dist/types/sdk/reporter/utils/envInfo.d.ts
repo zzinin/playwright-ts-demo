a31bbdb7fd3965c86616dfa4dc1e2f6f9e42fe5e
RR <zzinin@yahoo.com>
2025-08-25 16:59:19 +0100

    removed sensitive,temp and system specific files to be pushed to github

--- node_modules/allure-js-commons/dist/types/sdk/reporter/utils/envInfo.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * The implementation is based on https://github.com/gagle/node-properties.
- */
-import type { EnvironmentInfo } from "../../types.js";
-export declare const stringifyEnvInfo: (envInfo: EnvironmentInfo) => string;
-export declare const parseEnvInfo: (input: string) => EnvironmentInfo;
--- node_modules/allure-js-commons/dist/types/sdk/reporter/utils/labels.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Label } from "../../../model.js";
-export declare const getEnvironmentLabels: () => Label[];
-export declare const getHostLabel: () => Label;
-export declare const getThreadLabel: (userProvidedThreadId?: string) => Label;
-export declare const getPackageLabel: (filepath: string) => Label;
-export declare const getLanguageLabel: () => Label;
-export declare const getFrameworkLabel: (framework: string) => Label;
